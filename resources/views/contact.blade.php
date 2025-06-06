510263f3c103736b71e9ff37e0c5a77ed20656cd
michelicorsino <micheli.magalhaes@aluno.ifsp.edu.br>
2025-06-06 01:47:03 -0300

    Subindo projeto Laravel

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+@section('titlePage','Contatos')
+@section('contentPage')
+    <h1>Tela contatos</h1>
+    <a href="/">
+        <button>
+            Voltar
+        </button>
+    </a>
+@endsection
---
+++ resources/views/events/create.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.main')
+@section('titlePage','Criar Evento')
+@section('contentPage')
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $erro)
+                <li>{{ $erro }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+    <div id="event-create-container" class="col-md-6 offset-md-3">
+        <h1>Crie seu evento</h1>
+        <form action="/events" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
+                <label for="image">Imagem do Evento:</label>
+                <input type="file" id="image" name="image" class="form-control-file">
+            </div>
+            <div class="form-group">
+                <label for="title">Evento:</label>
+                <input type="text" class="form-control" id="title" name="title">
+            </div>
+            <div class="form-group">
+                <label for="date">Data do evento:</label>
+                <input type="date" class="form-control" id="date" name="date">
+            </div>
+            <div class="form-group">
+                <label for="city">Cidade:</label>
+                <input type="text" class="form-control" id="city" name="city">
+            </div>
+            <div class="form-group">
+                <label for="private">O evento é privado?</label>
+                <select class="form-control" id="private" name="private">
+                    <option value="0">Não</option>
+                    <option value="1">Sim</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="description">Descrição:</label>
+                <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento?"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="description">Adicione itens de infraestrutura:</label>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="Cadeiras"> Cadeiras
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="Palco"> Palco
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="Cerveja Gratis"> Cerveja Grátis
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="Open Food"> Open Food
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="Brindes"> Brindes
+                </div>
+            </div>
+            <input type="submit" value="Criar Evento" class="btn btn-primary">
+        </form>
+    </div>
+
+@endsection
