870d999546a965da5788496ffe26e0633e559993
Micheli Magalhães Corsino <92128570+michelicorsino@users.noreply.github.com>
2025-06-06 09:01:14 -0300

    Update main.blade.php
    mudando o cadastrar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -45,7 +45,7 @@
                                 <a href="/login" class="nav-link">Entrar</a>
                             </li>
                             <li class="nav-item">
-                                <a href="/register" class="nav-link">Cadastrar</a>
+                                <a href="/register" class="nav-link">Cadastrar-se</a>
                             </li>
                         @endguest
                     </ul>
