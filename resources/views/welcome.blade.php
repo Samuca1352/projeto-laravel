ec91bd557dd1d36ba62a4417803847dcfcd5b10b
Samuel Damasceno Cardoso <82730480+Samuca1352@users.noreply.github.com>
2025-06-13 09:06:48 -0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 
-@section('title', 'Gastos')
+@section('title', 'Controle de Despesas')
 
 @section('content')
 
@@ -15,13 +15,15 @@
     <h2>Buscando por: {{ $search }}</h2>
     @else
     <h2>Próximas Despesas</h2>
-    <p class="subtitle">Veja as despesas dos próximos dias</p>
+    {{-- NOVO: Exibe o nome do usuário se ele estiver logado --}}
+    @auth
+        <h4 class="subtitle text-center my-3">Bem-vindo(a), {{ auth()->user()->name }}!</h4>
+    @endauth
+    <p class="subtitle">Veja as despesas com vencimento próximo</p>
     @endif
     <div id="cards-container" class="row">
         @foreach($despesas as $despesa)
         <div class="card col-md-3">
-            {{-- A imagem foi removida, você pode colocar um ícone padrão se quiser --}}
-            {{-- <img src="/img/despesas/{{ $despesa->image }}" alt="{{ $despesa->descricao }}"> --}}
             <div class="card-body">
                 <p class="card-date">{{ date('d/m/Y', strtotime($despesa->data_vencimento)) }}</p>
                 <h5 class="card-title">{{ $despesa->descricao }}</h5>
@@ -33,7 +35,7 @@
         @if(count($despesas) == 0 && $search)
             <p>Não foi possível encontrar nenhuma despesa com '{{ $search }}'! <a href="/">Ver todas</a></p>
         @elseif(count($despesas) == 0)
-            <p>Não há despesas disponíveis no momento.</p>
+            <p>Não há despesas pendentes no momento.</p>
         @endif
     </div>
 </div>
